c2f2d9b21d211063a7b3ece6b5a77b90ad748c91
X_Tu <1206294056@qq.com>
2022-09-29 10:00:58 +0800

    update LibcSearcher.py

--- LibcSearcher.py
+++ LibcSearcher.py
@@ -7,13 +7,16 @@ import sys
 
 
 class LibcSearcher(object):
-    def __init__(self, func=None, address=None):
-        self.condition = {}
-        if func is not None and address is not None:
+    def __init__(self, *args):
+        self.conditions = {}
+        for func, address in args:
             self.add_condition(func, address)
         self.libc_database_path = os.path.join(
-            os.path.realpath(os.path.dirname(__file__)), "libc-database/db/")
-        self.db = ""
+            os.path.realpath(os.path.dirname(__file__)),
+            "libc-database/db/"
+        )
+        self.database = None
+        self.base_addr = None
 
     def add_condition(self, func, address):
         if not isinstance(func, str):
@@ -22,101 +25,106 @@ class LibcSearcher(object):
         if not isinstance(address, int):
             print("The address should be an int number")
             sys.exit()
-        self.condition[func] = address
+        self.conditions[func] = address
 
     #Wrapper for libc-database's find shell script.
     def decided(self):
-        if len(self.condition) == 0:
+        if not self.conditions:
             print("No leaked info provided.")
             print("Please supply more info using add_condition(leaked_func, leaked_address).")
             sys.exit(0)
 
-        res = []
-        for name, address in self.condition.items():
-            addr_last12 = address & 0xfff
-            res.append(re.compile("^%s .*%x" % (name, addr_last12)))
-
-        db = self.libc_database_path
-        files = []
+        results = []
         # only read "*.symbols" file to find
-        for _, _, f in os.walk(db):
-            for i in f:
-                files += re.findall('^.*symbols$', i)
-        
-        result = []
-        for ff in files:
-            fd = open(db + ff, "rb")
-            data = fd.read().decode(errors='ignore').split("\n")
-            for x in res:
-                if any(map(lambda line: x.match(line), data)):
-                    result.append(ff)
-            fd.close()
+        for fname in os.listdir(self.libc_database_path):
+            if not fname.endswith('.symbols'):
+                continue
+            fpath = os.path.join(self.libc_database_path, fname)
+            # print('calcing', fpath)
+            delta = self._calc_base(fpath)
+            if delta is not None:
+                self.base_addr = delta
+                results.append(fname)
 
-        if len(result) == 0:
+        if not results:
             print("No matched libc, please add more libc or try others")
             sys.exit(0)
 
-        if len(result) > 1:
-            print("Multi Results:")
-            for x in range(len(result)):
-                print("%2d: %s" % (x, self.pmore(result[x])))
-            print("Please supply more info using \n\tadd_condition(leaked_func, leaked_address).")
-            while True:
-                in_id = input(
-                    "You can choose it by hand\nOr type 'exit' to quit:")
-                if in_id == "exit" or in_id == "quit":
-                    sys.exit(0)
-                try:
-                    in_id = int(in_id)
-                    self.db = result[in_id]
-                    break
-                except:
-                    continue
+        if len(results) == 1:
+            result = results[0]
         else:
-            self.db = result[0]
-        print("[+] %s be choosed." % self.pmore(self.db))
+            print("Multi Results:")
+            for i, result in enumerate(results):
+                print(f"{i+1:2d}: {self.pmore(result)}")
+            print("Please supply more info using \n\tadd_condition(leaked_func, leaked_address).")
+            result = results[self._get_input() - 1]
+        print(f"[+] {self.pmore(result)} be choosed.")
+        fpath = os.path.join(self.libc_database_path, result)
+        with open(fpath, encoding='utf-8', errors='ignore') as f:
+            database = {}
+            for line in f:
+                name, addr = line.split()
+                database[name] = int(addr, base=16)
+            self.database = database
 
     def pmore(self, result):
         result = result[:-8]  # .strip(".symbols")
-        fd = open(self.libc_database_path + result + ".info")
-        info = fd.read().strip()
-        return("%s (id %s)" % (info, result))
+        fpath = os.path.join(self.libc_database_path, result + '.info')
+        with open(fpath, encoding='utf-8', errors='ignore') as f:
+            info = f.read().strip()
+            return f"{info} (id {result})"
 
     #Wrapper for libc-database's dump shell script.
     def dump(self, func=None):
-
-        if not self.db:
+        if not self.database:
             self.decided()
-        db = self.libc_database_path + self.db
-        fd = open(db, "rb")
-        data = fd.read().decode(errors='ignore').strip("\n").split("\n")
         if not func:
-            result = {}
-            func = [
-                "__libc_start_main_ret", "system", "dup2", "read", "write",
-                "str_bin_sh"
-            ]
-            for ff in func:
-                for d in data:
-                    f = d.split(" ")[0]
-                    addr = d.split(" ")[1]
-                    if ff == f:
-                        result[ff] = int(addr, 16)
-            for k, v in result.items():
-                print(k, hex(v))
-            return result
+            results = {}
+            funcs = ["__libc_start_main_ret", "system", "dup2", "read", "write", "str_bin_sh"]
 
-        for d in data:
-            f = d.split(" ")[0]
-            addr = d.split(" ")[1]
-            if func == f:
-                return int(addr, 16)
+            for name in funcs:
+                if name in self.database:
+                    addr = self.database[name] + self.base_addr
+                    # print(name, hex(addr))
+                    results[name] = addr
+            return results
+
+        if func in self.database:
+            return self.database[func] + self.base_addr
 
         print("No matched, Make sure you supply a valid function name or just add more libc.")
         return 0
 
+    def _calc_base(self, fpath):
+        with open(fpath, encoding='utf-8', errors='ignore') as f:
+            base = None
+            for line in f:
+                name, addr = line.split()
+                addr = int(addr, base=16)
+                if name in self.conditions:
+                    condition = self.conditions[name]
+                    if condition & 0xfff != addr & 0xfff:
+                        return None
+                    if base is None:
+                        base = condition - addr
+                    elif base != condition - addr:
+                        return None
+            return base
+
+    def _get_input(self):
+        while True:
+            cmd = input("You can choose it by hand\nOr type 'exit' to quit: ")
+            if cmd in ('exit', 'quit'):
+                sys.exit(0)
+            try:
+                return int(cmd)
+            except ValueError:
+                pass
+
 
 if __name__ == "__main__":
-    obj = LibcSearcher("fgets", 0x7ff39014bd90)
-    print("[+]system  offset: ", hex(obj.dump("system")))
-    print("[+]/bin/sh offset: ", hex(obj.dump("str_bin_sh")))
+    obj = LibcSearcher(('puts', 0x00007f19e6e3a970), ('alarm', 0x00007f19e6e9e4f0))
+    # print(obj.dump())
+    print(hex(obj.dump('puts')))
+    # print("[+]system  offset: ", hex(obj.dump("system")))
+    # print("[+]/bin/sh offset: ", hex(obj.dump("str_bin_sh")))
